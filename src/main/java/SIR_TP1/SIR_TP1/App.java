eb6b85630f88057009b198b84218656ac11a674b
mikaelphilip <12000209@e010m03.istic.univ-rennes1.fr>
2016-01-06 16:02:31 +0100

    new

---
+++ src/main/java/SIR_TP1/SIR_TP1/App.java
@@ -0,0 +1,18 @@
+package SIR_TP1.SIR_TP1;
+
+/**
+ * Classe qui sert à faire joli 
+ *
+ */
+public class App 
+{
+	public int useless;
+	
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+    
+    public void uselessmethod(){
+    	System.out.println("code mort");
+    }
+}
